ddfac1eb3e2bae0cb26e0d08a29fffe1635e66f7
Dandy Candra <dandycandrapratama@gmail.com>
2023-08-19 22:47:31 +0700

    adjust table features

--- components/Inventory/TableProducts.vue
+++ components/Inventory/TableProducts.vue
@@ -1,31 +1,66 @@
 <script setup lang="ts">
 import type IResponseProducts from "../../types/responseProducts.types";
 
-const props = defineProps<{
-  rowsData?: IResponseProducts[];
-  nameValue: string;
-}>();
+const toast = useToast();
+const { rowsData, nameValue, currentPage, totalPage, onNext, onPrev, refetch } =
+  defineProps<{
+    rowsData?: IResponseProducts[];
+    nameValue: string;
+    currentPage: number;
+    totalPage: number;
+    onNext: () => void;
+    onPrev: () => void;
+    refetch: () => void;
+  }>();
 
-defineEmits(["update:nameValue"]);
+defineEmits(["update:nameValue", "update:modalState"]);
 
-const { rowsData, nameValue } = props;
+const handleDeleteProducts = async (id: string) => {
+  const { status, error } = await useRequestSend({
+    endpoint: `products/${id}`,
+    method: "DELETE",
+  });
+
+  if (status.value === "error") {
+    console.log(error.value);
+    toast.add({
+      title: "Failed Delete Products",
+      description: error.value?.message,
+      icon: "i-heroicons-information-circle",
+      color: "red",
+    });
+  }
+
+  if (status.value === "success") {
+    toast.add({
+      title: "Success Delete Products",
+      icon: "i-heroicons-check-badge",
+      color: "green",
+    });
+    refetch();
+  }
+};
 
 const columnsData = [
   {
     key: "productName",
     label: "PRODUCT NAME",
+    sortable: true,
   },
   {
     key: "category",
     label: "CATEGORY",
+    sortable: true,
   },
   {
     key: "price",
     label: "PRICE",
+    sortable: true,
   },
   {
     key: "stock",
     label: "STOCK",
+    sortable: true,
   },
   {
     key: "actions",
@@ -44,13 +79,14 @@ const itemsDropdown = (row: any) => [
     {
       label: "Edit",
       icon: "i-heroicons-pencil-square-20-solid",
-      click: () => console.log("Edit", row.id),
+      click: () => console.log(row.id),
     },
   ],
   [
     {
       label: "Delete",
       icon: "i-heroicons-trash-20-solid",
+      click: () => handleDeleteProducts(row.id),
     },
   ],
 ];
@@ -61,38 +97,33 @@ const itemsDropdown = (row: any) => [
     <div class="flex flex-row justify-between items-center">
       <h1 class="text-[20px] text-[#383E49] font-medium">Products</h1>
 
-      <div class="flex flex-row gap-5">
-        <UButton
-          color="blue"
-          variant="solid"
-          class="h-[40px]"
-          icon="i-heroicons-plus-circle"
-          >Add Product</UButton
-        >
-        <UButton
-          color="white"
-          variant="outline"
-          icon="i-heroicons-funnel"
-          class="h-[40px] hover:bg-[#D0D3D9]"
-          >Fillter</UButton
-        >
-      </div>
+      <UButton
+        color="blue"
+        variant="solid"
+        class="h-[40px]"
+        icon="i-heroicons-plus-circle"
+        @click="$emit('update:modalState', true)"
+        >Add Product</UButton
+      >
     </div>
 
-    <UInput
-      :value="nameValue"
-      @input="$emit('update:nameValue', $event.target.value)"
-      icon="i-heroicons-magnifying-glass-20-solid"
-      size="md"
-      color="gray"
-      :trailing="false"
-      placeholder="Search Products"
-      class="mt-5 w-[350px]"
-    />
+    <div class="w-[500px]">
+      <UInput
+        :value="nameValue"
+        @input="$emit('update:nameValue', $event.target.value)"
+        icon="i-heroicons-magnifying-glass-20-solid"
+        size="md"
+        color="gray"
+        :trailing="false"
+        placeholder="Search Products"
+        class="mt-5"
+      />
+    </div>
 
     <UTable
       :rows="rowsData"
       :columns="columnsData"
+      :sort="{ column: 'productName', direction: 'asc' }"
       @select=""
       class="mt-5 mb-10"
     >
@@ -106,5 +137,21 @@ const itemsDropdown = (row: any) => [
         </UDropdown>
       </template>
     </UTable>
+
+    <div class="flex flex-row justify-center items-center gap-5">
+      <UButton
+        icon="i-heroicons-arrow-small-left-20-solid"
+        color="blue"
+        variant="soft"
+        @click="onPrev"
+      />
+      <p class="font-medium">{{ currentPage }} / {{ totalPage }}</p>
+      <UButton
+        icon="i-heroicons-arrow-small-right-20-solid"
+        color="blue"
+        variant="soft"
+        @click="onNext"
+      />
+    </div>
   </div>
 </template>
