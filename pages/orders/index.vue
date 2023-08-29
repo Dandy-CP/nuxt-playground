d40364c94436db940ce3d6860c2d00b1530f91e3
Dandy Candra <dandycandrapratama@gmail.com>
2023-08-29 15:28:45 +0700

    add components to page

--- pages/orders/index.vue
+++ pages/orders/index.vue
@@ -1,3 +1,50 @@
+<script setup lang="ts">
+import type IResponseOrder from "types/responseOrders.type";
+
+const page = ref<number>(1);
+const limit = ref<number>(10);
+const valueSearch = ref<string>("");
+
+const {
+  data: ordersData,
+  pending,
+  refresh,
+} = await useRequestApi.get<IResponseOrder>({
+  endpoint: "transaction",
+  query: {
+    page: page,
+    limit: limit,
+    productName: valueSearch,
+  },
+});
+
+const onNext = () => {
+  if (page.value + 1 <= ordersData?.value!.meta.totalPages) {
+    page.value = page.value + 1;
+  }
+};
+
+const onPrev = () => {
+  if (page.value != 1) {
+    page.value = page.value - 1;
+  }
+};
+
+useHead({
+  title: "Orders",
+});
+</script>
+
 <template>
-  <h1>Orders Page</h1>
+  <div class="p-5">
+    <OrdersOverall />
+
+    <OrdersTableOrders
+      :ordersData="(ordersData as IResponseOrder)"
+      :currentPage="ordersData!.meta.currentPage"
+      :totalPage="ordersData!.meta.totalPages"
+      :onNext="onNext"
+      :onPrev="onPrev"
+    />
+  </div>
 </template>
